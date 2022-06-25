92792ffbe660333cc3dc4d7dcd96f9988eead02e
Ceyhun ASLAN <ceyhunaaslan@gmail.com>
2022-06-25 16:41:04 +0300

    Proje dosyası ekle.

---
+++ My_Pizzaria/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using My_Pizzaria.Models;
+
+namespace My_Pizzaria.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<PizzaOrder> PizzaOrders { get; set; }
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
